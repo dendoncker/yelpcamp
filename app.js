e694cadb087b9a0a72bfc969d735d3ae590880f0
Francois Dendoncker <fdendoncker@jcu.cz>
2023-11-29 19:24:24 +0100

    still preparing to deploy

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ if (process.env.NODE_ENV !== "production") {
 
 const express = require('express');
 const app = express();
-
 const helmet = require('helmet');
 
 const path = require('path');
-const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 
 const session = require('express-session');
@@ -26,16 +24,8 @@ const usersRouter = require('./routes/users.js');
 
 const flash = require('connect-flash');
 
-const mongoUrl = 'mongodb+srv://dendonckerfrancois:z4Zlsy9hEtXV4tcF@mycluster.bu2ubl7.mongodb.net/?retryWrites=true&w=majority';
-// const mongoUrl = 'mongodb://127.0.0.1:27017/yelpCamp'
-
-mongoose.connect(mongoUrl);
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-    console.log('Database connected');
-});
+const mongoConnexion = require('./utilities/mongoconnexion.js');
+mongoConnexion();
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
@@ -44,38 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-const scriptSrcUrls = [
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",
-    "https://api.mapbox.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://kit.fontawesome.com/",
-    "https://cdnjs.cloudflare.com/",
-    "https://cdn.jsdelivr.net",
-];
-
-const styleSrcUrls = [
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
-    "https://api.mapbox.com/",
-    "https://fonts.googleapis.com/",
-    "https://kit-free.fontawesome.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://use.fontawesome.com/"
-];
-
-const connectSrcUrls = [
-    "https://api.mapbox.com/",
-    "https://a.tiles.mapbox.com/",
-    "https://b.tiles.mapbox.com/",
-    "https://events.mapbox.com/events/",
-];
-
-const imgSrcUrls = [
-    "https://images.unsplash.com/",
-    "https://res.cloudinary.com/df4rzp9oa/"
-];
-
-const fontSrcUrls = [
-];
+const { connectSrcUrls, fontSrcUrls, scriptSrcUrls, imgSrcUrls, styleSrcUrls } = require('./utilities/helmetconfig.js');
 
 app.use(
     helmet.contentSecurityPolicy({
@@ -91,9 +50,11 @@ app.use(
     },
     ));
 
+const secret = process.env.SECRET || 'ewq#$@^47623';
+
 const sessionConfig = {
     name: 'session',
-    secret: 'mykey',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
